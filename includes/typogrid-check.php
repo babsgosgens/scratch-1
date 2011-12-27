edb198ad4516d519922b3da6ab3ac43aed1bed44
Cristina Solana <cs@nightshiftcreative.com>
2011-12-27 01:12:15 -0500

    add quick typographic test grid, sucks, but it'll do for now. :P

---
+++ includes/typogrid-check.php
@@ -0,0 +1,3 @@
+<div class="typo cf">
+	<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+</div>
