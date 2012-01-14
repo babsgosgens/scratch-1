0a779cbfb50086bcb40a57f9375d44b9f772dcf9
Cristina Solana <cs@nightshiftcreative.com>
2012-01-13 20:19:59 -0500

    move widgets to widgets dir

---
+++ widgets/alert.php
@@ -0,0 +1,11 @@
+<?php include("includes/top.php"); ?>
+	<div class="alert-box slide">
+		I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)
+		<a class="close" href="">&times;</a>
+	</div>
+
+	<div class="alert-box">
+		I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)
+		<a class="close" href="">&times;</a>
+	</div>
+<?php include("includes/bottom.php"); ?>
---
+++ widgets/grid.php
@@ -0,0 +1,74 @@
+<?php include("includes/top.php"); ?>
+		<header class="group">
+			<div class="span-12">
+				12
+			</div>
+			<div class="span-12">
+				12
+			</div>
+		</header>
+
+		<nav class="group">
+			<article class="span-3">
+				3
+			</article>
+			<aside class="span-5">
+				5
+			</aside>
+			<article class="span-3">
+				3
+			</article>
+			<aside class="span-5">
+				5
+			</aside>
+			<article class="span-3">
+				3
+			</article>
+			<aside class="span-5">
+				5
+			</aside>
+		</nav>
+
+		<section class="group">
+			<article class="span-8">
+				8
+			</article>
+			<aside class="span-8">
+				8
+			</aside>
+			<article class="span-8">
+				8
+			</article>
+		</section>
+
+		<section class="group">
+			<article class="span-6">
+				6
+			</article>
+			<aside class="span-6">
+				6
+			</aside>
+			<article class="span-6">
+				6
+			</article>
+			<aside class="span-6">
+				6
+			</aside>
+		</section>
+
+		<footer class="group">
+			<article class="span-6">
+				6
+			</article>
+			<aside class="span-18">
+				18
+			</aside>
+		</footer>
+
+		<footer class="group">
+			<article class="span-24">
+				24
+			</article>
+		</footer>
+
+<?php include("includes/bottom.php"); ?>
