920f791089200caad6de89e14206f4406c5d520f
Cristina Solana <cs@nightshiftcreative.com>
2012-01-21 12:22:22 -0500

    moved files in root to src

---
+++ src/_partials/bottom.php
@@ -0,0 +1,8 @@
+	<!-- end test #container -->
+	</div>
+	<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery.alerts.js"></script>
+	<!-- yepnope, for when necessary: conditional loads or large amounts of js files
+	<script type="text/javascript" charset="utf-8" src="js/main.js"></script> -->
+	</body>
+</html>
---
+++ src/_partials/widgets/button.php
@@ -0,0 +1,4 @@
+
+	<a href="#" class="button">anchor</a>
+	<button type="button" class="button">button</button>
+	<input type="button" value="input" class="button">
