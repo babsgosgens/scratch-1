f2918897679717e01d8a4459782135528a363010
Cristina Solana <cs@nightshiftcreative.com>
2012-01-28 18:40:48 -0500

    add p elem to text in alert

--- src/_partials/widgets/alert.php
+++ src/_partials/widgets/alert.php
@@ -1,10 +1,10 @@
 
 	<div class="alert-box slide">
-		I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)
+		<p>I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)</p>
 		<a class="close" href="">&times;</a>
 	</div>
 
 	<div class="alert-box">
-		I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)
+		<p>I fade out by default. Add 'slide' class and I'll lose my margins and slide instead. :)</p>
 		<a class="close" href="">&times;</a>
 	</div>
