5d1b86ab97f4119069c80cd671bfe85c795f5c42
Cristina Solana <cs@nightshiftcreative.com>
2012-01-31 19:32:01 -0500

    add jquery tools

--- src/_partials/bottom.php
+++ src/_partials/bottom.php
@@ -1,9 +1,12 @@
 	<!-- end test #container -->
 	</div>
-	<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/jquery.alerts.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/jquery.uniform.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/scripts.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<!-- Plugins -->
+	<script src="http://cdn.jquerytools.org/1.2.6/all/jquery.tools.min.js"></script>
+	<script src="js/jquery.uniform.min.js"></script>
+	<!-- custom -->
+	<script src="js/jquery.alerts.js"></script>
+	<script src="js/scripts.js"></script>
 	<!-- yepnope, for when necessary: conditional loads or large qty of js files
 	<script type="text/javascript" charset="utf-8" src="js/main.js"></script> -->
 	</body>
