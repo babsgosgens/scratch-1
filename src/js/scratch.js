ecd8cb6d7710a77e3992318ab12be12ee9bc8d2e
Cristina Solana <cs@nightshiftcreative.com>
2012-04-03 07:52:51 -0400

    reorganizing js

---
+++ src/js/scratch.js
@@ -0,0 +1,52 @@
+$(function() {
+
+	// Uniform Form Prettifier
+
+	$("select, textarea, input:checkbox, input:radio, input:text").uniform();
+
+    // Mobile + Menu
+	// Highly inspired by Seth Warburton @nternetinspired
+
+    var menu = $('#global-nav'),
+        menuToggle = $('#toggler');
+
+    menu.addClass('hidden');
+    menu.removeClass('visuallyhidden');
+
+    menuToggle.click(function(e) {
+        menu.slideToggle();
+        e.preventDefault();
+    });
+
+    function menuSwitch(width) {
+        width = $(window).width();
+        if (width > 750){
+            menu.removeClass('hidden');
+        } else {
+            menu.addClass('hidden');
+        }
+    }
+    menuSwitch();
+
+    // Chris Coyier's elegant screensize test http://css-tricks.com/resolution-specific-stylesheets
+
+    $(function() {
+        menuSwitch($(window).width());
+        $(window).resize(function() {
+            menuSwitch($(window).width());
+        });
+    });
+
+    // Alerts
+
+    $('a.close').click(function(e) {
+        var $target=$(e.target);
+        if ($target.parent().is('.slide')) {
+            $target.parent('.alert-box').slideUp();
+        } else {
+            $target.parent('.alert-box').fadeOut();
+        }
+        e.preventDefault();
+    });
+
+});
