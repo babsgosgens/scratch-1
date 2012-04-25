8c3ec3c4893ca8a2342e0eb3771ee7140e522504
Cristina <cs@nightshiftcreative.com>
2012-04-25 12:51:19 -0400

    modified html tag, moved charset dec from meta to header, minified css, reflow of js and minification

--- src/_partials/top.php
+++ src/_partials/top.php
@@ -1,10 +1,11 @@
+<?php header('Content-type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html>
 <!--[if lte IE 7]>	<![endif]-->
 <!--[if IE 7]>		<![endif]-->
 <!--[if IE 8]>		<![endif]-->
 <!--[if (gt IE 9)|!(IE)]> <!--> <html xml:lang="en" lang="en" class="no-js"> <!--<![endif]-->
 	<head>
-		<meta charset="utf-8">
+		<!--<meta http-equiv="Content-Type" content="text/html; charset=utf-8" >-->
 		<meta name="viewport" content="width=device-width" />
 		<title>Scratch: The Agnostic</title>
 		<meta name="description" content="Describe me." />
@@ -12,7 +13,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		-->
 		<link rel="stylesheet" href="css/all.css?v=2">
-
+		<script src="js/libs/modernizr.custom.js"></script>
 		<!-- NOTE: Plan on supporting IE lt 9 beyond serving it content? Or fight the good fight and delete it. -->
 		<!--[if lt IE 9]>
 			<link rel="stylesheet" href="stylesheets/ie.css">
@@ -23,6 +24,5 @@
 		<!--[if lt IE 9]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-		<script src="js/modernizr.custom.js"></script>
 	</head>
 	<body>
--- src/js/modload.js
+++ src/js/modload.js
@@ -1,8 +1,6 @@
 Modernizr.load([
-	'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
-	'http://cdn.jquerytools.org/1.2.6/all/jquery.tools.min.js',
-	'js/jquery.uniform.min.js',
-	'js/scratch.js', // compile all non-cdn to one file: all.js when applicable
+	'js/xlibs.min.js',		// jquery, uniform
+	'js/scratch.min.js',		// custom
 	{
 		test:  Modernizr.mq,
 		nope: 'https://github.com/scottjehl/Respond/blob/master/respond.min.js'
