246ed7ebe743d2d6156f19317633a2b46fe9cee7
Cristina <cs@nightshiftcreative.com>
2012-05-23 13:41:29 -0400

    playing around with shelves idea

--- src/shelves.php
+++ src/shelves.php
@@ -3,25 +3,35 @@
 
 <!-- begin test #container -->
 
-	<div id="container" class="case microcf">
+	<div id="container" class="case">
 
-		<header class="shelf masthead microcf">
-			<div class="block block-logo microcf">I am a Logo</div>
-			<div class="block block-social microcf">And I am Social. Ha.</div>
+		<header class="shelf two-up masthead">
+			<div class="block block-logo">I am a Logo</div>
+			<div class="block block-social">And I am Social. Ha.</div>
 		</header>
 
 <!-- if same, throughout site, end top include -->
-		<nav class="shelf microcf">
+		<nav class="shelf">
 
 		</nav>
-		<section class="shelf two-up content-main microcf"> <!-- is this where the block layout hook should go? I tend to think so atm. How to deal with multiple tag elements. --> 
-			<article class="block block-message microcf">
+		<section class="shelf two-up content-main"> <!-- is this where the block layout hook should go? I tend to think so atm. How to deal with multiple tag elements. --> 
+			<article class="block block-message">
 				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 			</article>
-			<aside class="block block-cta microcf">
+			<aside class="block block-cta">
 				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 			</aside>
-			<div class="block block-gallery microcf">
+			<div class="block block-gallery">
+				<ul class="two-up">
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far away.</p></figcaption></li>
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far far away.</p></figcaption></li>
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far far far away.</p></figcaption></li>
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far far far far away.</p></figcaption></li>
+					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far far far far far away.</p></figcaption></li>
+				</ul>
+			</div>
+			<div class="block block-gallery">
 				<ul class="two-up">
 					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
 					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
@@ -31,31 +41,21 @@
 					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
 				</ul>
 			</div>
-			<div class="block block-gallery microcf">
-				<ul class="two-up">
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-					<li><figure><img src="http://placehold.it/160/ff0066/ffffff"></figure><figcaption><p>Far far away.</p></figcaption></li>
-				</ul>
-			</div>
-			<footer class="block block-author microcf">
-				I be a content's footer
+			<footer class="block block-author">
+				I be a content's footer. I end [odd]ly. :D
 			</footer>
 		</section>
- 		<section class="shelf two-up global-support microcf"> 
-			<div class="block block-news microcf">
+ 		<section class="shelf two-up global-support"> 
+			<div class="block block-news">
 				<h5>Heading News</h5>
 				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 			</div>
-			<div class="block block-blog microcf">
+			<div class="block block-blog">
 				<h5>Heading Blog</h5>
 				<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
 			</div>
 		</section>
-		<footer class="shelf microcf">
+		<footer class="shelf">
 			blah blah footer blah
 		</footer>
 
