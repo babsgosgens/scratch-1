4c4fae6827642b77acd8fa89435e4108d724730a
Cristina Solana <cs@nightshiftcreative.com>
2012-11-24 23:22:18 -0500

    remove and replace files

--- boilerplate/templates/project/js/modload.js
+++
@@ -1,8 +0,0 @@
-Modernizr.load([
-	'js/xlibs.min.js',		// jquery, uniform
-	'js/scratch.min.js',		// custom
-	{
-		test:  Modernizr.mq,
-		nope: 'https://github.com/scottjehl/Respond/blob/master/respond.min.js'
-	}
-]);
